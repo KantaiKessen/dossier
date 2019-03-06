36d1e52d4339613f0c4caad522341dfe74c5152c
galxymanonearth <37821485+galxymanonearth@users.noreply.github.com>
2019-03-05 20:42:50 -0800

    Add files via upload

--- src/SecurityFeatures.java
+++ src/SecurityFeatures.java
@@ -4,8 +4,15 @@ class SecurityFeatures{
             java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
             byte[] array = md.digest(md5.getBytes());
             StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < array.length; ++i) {
+            int end =0;
+            int i=0;
+            while (end==0) {
+                
                 sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+                if(i<array.length)
+                {
+                    end++;
+                }
             }
             return sb.toString();
         } catch (java.security.NoSuchAlgorithmException e) {
